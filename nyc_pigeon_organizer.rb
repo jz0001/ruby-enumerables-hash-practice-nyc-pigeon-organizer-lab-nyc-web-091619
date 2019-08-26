d511a3224f2d5b28241fb9c4a13be5587dcaf7f5
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-26 07:55:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,60 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data2 = {}
+  data.each do |key, value|
+    value.each do |key2, value2|
+      value2.each do |element|
+        
+        if !data2[element]
+          data2[element] = {}
+        end
+
+        if !data2[element][key]
+          data2[element][key] = []
+        end
+
+        data2[element][key].push(key2.to_s)
+
+      end
+    end
+  end
+  data2
 end
+
+
+# data = {
+#   "Theo" => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Subway"]
+#   },
+#   "Peter Jr." => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Library"]
+#   },
+#   "Lucky" => {
+#     :color => ["purple"],
+#     :gender => ["male"],
+#     :lives => ["Central Park"]
+#   },
+#   "Ms. K" => {
+#     :color => ["grey", "white"],
+#     :gender => ["female"],
+#     :lives => ["Central Park"]
+#   },
+#   "Queenie" => {
+#     :color => ["white", "brown"],
+#     :gender => ["female"],
+#     :lives => ["Subway"]
+#   },
+#   "Andrew" => {
+#     :color => ["white"],
+#     :gender => ["male"],
+#     :lives => ["City Hall"]
+#   },
+#   "Alex" => {
+#     :color => ["white", "brown"],
+#     :gender => ["male"],
+#     :lives => ["Central Park"]
+#   }
+# }
